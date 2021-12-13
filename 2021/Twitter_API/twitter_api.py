d3ed74b76a585486e5ac5056765324f68acc3081
Mehran Shakarami <mehran.shakarami@gmail.com>
2021-12-13 23:30:50 +0100

    twitter_api

---
+++ 2021/Twitter_API/twitter_api.py
@@ -0,0 +1,31 @@
+import tweepy
+import configparser
+import pandas as pd
+
+# read configs
+config = configparser.ConfigParser()
+config.read('config.ini')
+
+api_key = config['twitter']['api_key']
+api_key_secret = config['twitter']['api_key_secret']
+
+access_token = config['twitter']['access_token']
+access_token_secret = config['twitter']['access_token_secret']
+
+# authentication
+auth = tweepy.OAuthHandler(api_key, api_key_secret)
+auth.set_access_token(access_token, access_token_secret)
+
+api = tweepy.API(auth)
+
+public_tweets = api.home_timeline()
+
+# create dataframe
+columns = ['Time', 'User', 'Tweet']
+data = []
+for tweet in public_tweets:
+    data.append([tweet.created_at, tweet.user.screen_name, tweet.text])
+
+df = pd.DataFrame(data, columns=columns)
+
+df.to_csv('tweets.csv')
