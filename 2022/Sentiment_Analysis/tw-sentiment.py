2f3497f258b16492ea34fc554075de9ed4907223
Mehran Shakarami <mehran.shakarami@gmail.com>
2022-03-01 12:21:20 +0100

    Create tw-sentiment.py

---
+++ 2022/Sentiment_Analysis/tw-sentiment.py
@@ -0,0 +1,41 @@
+from transformers import AutoTokenizer, AutoModelForSequenceClassification
+from scipy.special import softmax
+
+# tweet = "@MehranShakarami today's cold @ home 😒 https://mehranshakarami.com"
+tweet = 'Great content! subscribed 😉'
+
+# precprcess tweet
+tweet_words = []
+
+for word in tweet.split(' '):
+    if word.startswith('@') and len(word) > 1:
+        word = '@user'
+    
+    elif word.startswith('http'):
+        word = "http"
+    tweet_words.append(word)
+
+tweet_proc = " ".join(tweet_words)
+
+# load model and tokenizer
+roberta = "cardiffnlp/twitter-roberta-base-sentiment"
+
+model = AutoModelForSequenceClassification.from_pretrained(roberta)
+tokenizer = AutoTokenizer.from_pretrained(roberta)
+
+labels = ['Negative', 'Neutral', 'Positive']
+
+# sentiment analysis
+encoded_tweet = tokenizer(tweet_proc, return_tensors='pt')
+# output = model(encoded_tweet['input_ids'], encoded_tweet['attention_mask'])
+output = model(**encoded_tweet)
+
+scores = output[0][0].detach().numpy()
+scores = softmax(scores)
+
+for i in range(len(scores)):
+    
+    l = labels[i]
+    s = scores[i]
+    print(l,s)
+
