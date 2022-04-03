2a0ace34ce59ca5dc999c5d37106ba8173308041
Mehran Shakarami <mehran.shakarami@gmail.com>
2022-04-03 20:03:41 +0200

    Create snscrape

---
+++ 2022/snscrape/tweets.py
@@ -0,0 +1,22 @@
+import snscrape.modules.twitter as sntwitter
+import pandas as pd
+
+query = "(from:elonmusk) until:2020-01-01 since:2010-01-01"
+tweets = []
+limit = 5000
+
+
+for tweet in sntwitter.TwitterSearchScraper(query).get_items():
+    
+    # print(vars(tweet))
+    # break
+    if len(tweets) == limit:
+        break
+    else:
+        tweets.append([tweet.date, tweet.user.username, tweet.content])
+        
+df = pd.DataFrame(tweets, columns=['Date', 'User', 'Tweet'])
+print(df)
+
+# to save to csv
+# df.to_csv('tweets.csv')
